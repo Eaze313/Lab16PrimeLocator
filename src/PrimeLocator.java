f77e3836919ad2e0c1969fb733300f46d656c137
Anthony Parker <anthonymparker1@yahoo.com>
2017-02-25 21:25:47 -0500

    Added to Main

--- src/PrimeLocator.java
+++ src/PrimeLocator.java
@@ -1,20 +1,44 @@
+import java.util.Scanner;
+
 /**
  * Created by james on 2/23/17.
  */
 public class PrimeLocator {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String answer = "";
+        System.out.println("Let's locate some primes!");
+        System.out.println();
+
+
+        System.out.println("This application will find you any prime, " +
+                "in order, from first prime number on.");
+
+        int[] num = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
+                53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
+        do {
+            int i;
+
+            System.out.print("Which prime number are you looking for? ");
+            i = scan.nextInt();
+
+            System.out.println("The number " + i + " prime is " + num[i - 1] + ". ");
+
+            System.out.print("Do you want to find another number? (y/n) ");
+
+            answer = scan.next().toLowerCase();
+
+        } while (answer.equals("y"));
+
+        scan.close();
+        System.out.println("\nGoodbye");
     }
 
+    public static int[] primeNum(int[] num) {
 
-        public static int primeNum(int prime) {
-        for (int i = 2; i < 1000000;  i++) {
-            boolean isPrimeNumber = true;
+        for (int i = 0; i < num.length; i++) {
 
-
-
-        return prime;
         }
-
-
-
+        return num;
+    }
 }
--- src/PrimeLocatorTest.java
+++ src/PrimeLocatorTest.java
@@ -5,29 +5,34 @@ import org.junit.Test;
  * Created by james on 2/23/17.
  */
 public class PrimeLocatorTest {
+    @org.junit.Test
+    public void primeNum() throws Exception {
+
+    }
 
     @Test
-    public void test2() throws Exception {
-        Assert.assertEquals(2, PrimeLocator.primeNum(1));
+    public void test5() throws Exception {
+        Assert.assertEquals(5, PrimeLocator.primeNum(new int[]{5}));
     }
 
     @Test
     public void test11() throws Exception {
-        Assert.assertEquals(11, PrimeLocator.primeNum(5));
+        Assert.assertEquals(11, PrimeLocator.primeNum(new int[]{11}));
     }
 
     @Test
     public void test29() throws Exception {
-        Assert.assertEquals(29, PrimeLocator.primeNum(10));
+        Assert.assertEquals(29, PrimeLocator.primeNum(new int[]{10}));
     }
+
     @Test
     public void test41() throws Exception {
-        Assert.assertEquals(41, PrimeLocator.primeNum(13));
+        Assert.assertEquals(41, PrimeLocator.primeNum(new int[]{13}));
     }
 
     @Test
     public void test59() throws Exception {
-        Assert.assertEquals(59, PrimeLocator.primeNum(17));
+        Assert.assertEquals(59, PrimeLocator.primeNum(new int[]{17}));
     }
 
 }
